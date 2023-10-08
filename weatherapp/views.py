8ddf0578d33745f2e8808d82784b0afded4ce2db
sujit <sujitpulujkar01@gmail.com>
2023-10-08 19:46:34 +0530

    New updated

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -3,7 +3,7 @@ import requests
 import datetime
 
 def home(request):
-    custom_error_message = None  # Initialize a custom error message as None
+    custom_error_message = None  
 
     if request.method == 'POST':
         city = request.POST['city']
@@ -17,7 +17,6 @@ def home(request):
         dict_w = r.json()
         
         if 'error' in dict_w:
-            # Handle API error here if needed
             custom_error_message = "Please enter a correct city name"
         else:
             dict_weather = dict_w["current"]["temp_c"]
@@ -27,10 +26,7 @@ def home(request):
             return render(request, 'index1.html', {'city': city, 'temp': dict_weather, 'description': description, "icon": icon, 'custom_error_message': custom_error_message})
     
     except requests.exceptions.RequestException as e:
-        # Handle request error here if needed
         custom_error_message = f"Request failed: {e}"
     except ValueError as e:
-        # Handle JSON parsing error here if needed
         custom_error_message = f"JSON parsing error: {e}"
-
     return render(request, 'index1.html', {'custom_error_message': custom_error_message})
