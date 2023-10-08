3437a392045b07aa72d429dd7ddb1043488cc683
sujit <sujitpulujkar01@gmail.com>
2023-10-08 19:07:06 +0530

    updated app

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,65 +1,36 @@
-# from django.shortcuts import render
-# import requests
-# import datetime
-
-# # Create your views here.
-# def home(request):
-#     if 'city' in request.POST:
-#         city = request.POST['city']
-#     else:
-#         city ='solapur'
-
-#     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=5894e6e8e2afced9562bad1351aeeb17'
-#     PARAMS ={'units':'metric'}
-#     data = requests.get(url,params=PARAMS).json()
-#     description = data['weather'][0]['description']
-#     icon = data['weather'][0]['icon']
-#     temp = data['main']['temp']
-#     day = datetime.date.today()
-
-#     return render(request,'index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city })
-
-
-
-
-
-
-
-
-
-
-
-
-import json
 from django.shortcuts import render
-from django.contrib import messages
 import requests
-import urllib.request
 import datetime
 
-
 def home(request):
-          if request.method =='POST':
-                  city = request.POST['city']
-          else:
-                  city = 'solapur'
-          url = f"https://api.weatherapi.com/v1/current.json?key=c5e6022efbdc472583b143642232509&q={city}"
-          try:
-               r = requests.get(url)
-               dict_w = json.loads(r.text)
-               dict_weather = dict_w["current"]["temp_c"]
-               description = dict_w['current']["condition"]["text"]
-               icon = dict_w['current']["condition"]["icon"]
-               
-               print(dict_weather, description)
-               return render(request,'index1.html' ,{'city':city,'temp':dict_weather,'description':description,"icon":icon,} )
-          except requests.exceptions.RequestException as e:
-                 exception_occurred = True
-                 messages.error(request, f'Error: {e}')
-                 return render(request, 'index1.html', {'city': city, 'exception_occurred': exception_occurred})
-          except KeyError:
-               exception_occurred = True
-               messages.error(request, 'Entered data is not available from the API')
-               return render(request, 'index1.html', {'city': city, 'exception_occurred': exception_occurred})
-         
-               
+    custom_error_message = None  # Initialize a custom error message as None
+
+    if request.method == 'POST':
+        city = request.POST['city']
+    else:
+        city = 'solapur'
+    
+    url = f"https://api.weatherapi.com/v1/current.json?key=c5e6022efbdc472583b143642232509&q={city}"
+    
+    try:
+        r = requests.get(url)
+        dict_w = r.json()
+        
+        if 'error' in dict_w:
+            # Handle API error here if needed
+            custom_error_message = "Please enter a correct city name"
+        else:
+            dict_weather = dict_w["current"]["temp_c"]
+            description = dict_w['current']["condition"]["text"]
+            icon = dict_w['current']["condition"]["icon"]
+            
+            return render(request, 'index1.html', {'city': city, 'temp': dict_weather, 'description': description, "icon": icon, 'custom_error_message': custom_error_message})
+    
+    except requests.exceptions.RequestException as e:
+        # Handle request error here if needed
+        custom_error_message = f"Request failed: {e}"
+    except ValueError as e:
+        # Handle JSON parsing error here if needed
+        custom_error_message = f"JSON parsing error: {e}"
+
+    return render(request, 'index1.html', {'custom_error_message': custom_error_message})
